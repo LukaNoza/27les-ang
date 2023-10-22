905dc6cfd07366391aa9c72681fd35f63475aeee
Luka Noza <103760971+LukaNoza@users.noreply.github.com>
2023-10-22 17:33:05 +0400

    12345finish

---
+++ main.js
@@ -0,0 +1,69 @@
+// 1
+
+// const tableSize = 10;
+
+// for (let i = 1; i <= tableSize; i++) {
+//     let row = '';
+
+//     for (let j = 1; j <= tableSize; j++) {
+//         let product = i * j;
+
+//         row += `${i} x ${j} = ${product}\t`;
+//     }
+
+//     console.log(row);
+// }
+
+
+// 2
+
+// for (let number = 0; number <= 20; number++) {
+//     if (number % 2 === 0) {
+//         console.log(number);
+//     }
+// }
+
+
+// 3
+
+// let count = 0;
+
+// for (let number = 0; number <= 120; number++) {
+//     if (number % 4 === 0) {
+//         count++;
+//     }
+// }
+
+// console.log("The number of multiples of 4 between 0 and 120 is: " + count);
+
+
+// 4
+
+// const userNumber = parseInt(prompt("Enter a number:"));
+
+// let divisorCount = 0;
+
+// for (let i = 1; i <= userNumber; i++) {
+//     if (userNumber % i === 0) {
+//         divisorCount++;
+//     }
+// }
+
+// console.log(`The number ${userNumber} has ${divisorCount} divisor(s).`);
+
+
+// 5
+
+// const firstNumber = parseInt(prompt("Enter the first number:"));
+
+// const secondNumber = parseInt(prompt("Enter the second number:"));
+
+// if (firstNumber <= secondNumber) {
+//     for (let i = firstNumber; i <= secondNumber; i++) {
+//         console.log(i); 
+//     }
+// } else {
+//     for (let i = secondNumber; i <= firstNumber; i++) {
+//         console.log(i); 
+//     }
+// }
